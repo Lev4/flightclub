620768005d50d87d0bccf3032714110bc58dfdbe
Левченко Алексей Сергеевич <Levchenko.A.Sergeevic@sberbank.ru>
2020-11-15 21:43:51 +0300

    add scheduler

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import sched
+import time
 from datetime import datetime, timedelta
+
 from app.data_manager import DataManager
 from app.flight_search import FlightSearch
 from app.notification import NotificationManager
 from config import BOT_TOKEN, ADMIN_ID
+
+s = sched.scheduler(time.time, time.sleep)
 
 data_manager = DataManager()
 
@@ -15,6 +20,8 @@ data_package = {
     "six_month_from_today": datetime.now() + timedelta(days = (6 * 30)),
 }
 
+delay_seconds1 = 60
+delay_seconds2 = 100
 
 def search_flights(data_pack, period):
     for destination in data_pack["sheet_data"]:
@@ -44,7 +51,7 @@ def search_flights(data_pack, period):
 
                 msg += f"Перелет из {flight.origin_city}-{flight.origin_airport} "
                 msg += f"в {flight.destination_city}-{flight.destination_airport} за {flight.price} рублей.\n"
-                msg += f"Даты: {flight.out_date} ({flight.out_date_weekday}) - {flight.return_date} ({flight.return_date_weekday}).\n"
+                msg += f"Даты: {flight.out_date} ({flight.out_date_weekday}) - {flight.return_date} ({flight.return_date_weekday}).\n "
                 msg += f"Продолжительность {flight.duration_days} д"
 
                 data_pack["notification_manager"].send_sms(
@@ -52,5 +59,7 @@ def search_flights(data_pack, period):
                 )
 
 
-search_flights(data_package, "short")
-
+while True:
+    s.enter(delay_seconds1, 1, search_flights, kwargs = {"data_pack": data_package, "period": "short"})
+    s.enter(delay_seconds2, 2, search_flights, kwargs = {"data_pack": data_package, "period": "long"})
+    s.run()
